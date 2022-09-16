f55b0c2fe54aa91a92ca469bcf6c20d07d5814d0
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-09-16 16:34:19 +0530

    import java.util.Scanner; class aditya{     public static void main(String args[]){         String ch;         Scanner sc=new Scanner(System.in);         ch=sc.nextLine();         System.out.println(ch);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class aditya{
+    public static void main(String args[]){
+        String ch;
+        Scanner sc=new Scanner(System.in);
+        ch=sc.nextLine();
+        System.out.println(ch);
+    }
+}
