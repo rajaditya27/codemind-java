f7dcab018ffdc2cfd1950abb2e0210ad60df3b50
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-09-16 13:21:55 +0530

    import java.util.Scanner; class Ascii{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char n;         n=sc.nextLine().charAt(0);         System.out.println((int)n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Ascii{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char n;
+        n=sc.nextLine().charAt(0);
+        System.out.println((int)n);
+    }
+}
