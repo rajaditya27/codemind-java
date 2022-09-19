c20b02f34005f96cccc1698ed044359e39cd92b1
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-09-19 12:00:49 +0530

    import java.util.*; class Sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         // i=sc.nextInt();         if (ch=='v'||ch=='V')         System.out.println("Violet");         else if(ch=='i'||ch=='I')         System.out.println("Indigo");         else if(ch=='b'||ch=='B')         System.out.println("Blue");         else if(ch=='g'||ch=='G')         System.out.println("Green");         else if(ch=='y'||ch=='Y')         System.out.println("Yellow");         else if(ch=='o'||ch=='O')         System.out.println("Orange");         else if(ch=='r'||ch=='R')         System.out.println("Red");         else         System.out.println(-1);     } }

---
+++ Color_Code.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        // i=sc.nextInt();
+        if (ch=='v'||ch=='V')
+        System.out.println("Violet");
+        else if(ch=='i'||ch=='I')
+        System.out.println("Indigo");
+        else if(ch=='b'||ch=='B')
+        System.out.println("Blue");
+        else if(ch=='g'||ch=='G')
+        System.out.println("Green");
+        else if(ch=='y'||ch=='Y')
+        System.out.println("Yellow");
+        else if(ch=='o'||ch=='O')
+        System.out.println("Orange");
+        else if(ch=='r'||ch=='R')
+        System.out.println("Red");
+        else
+        System.out.println(-1);
+    }
+}
